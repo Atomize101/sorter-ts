c471714582ec1262aab3ad5bdc2929db241f4d45
Chris Patrick <atomize101@gmail.com>
2021-01-24 10:39:42 -0500

    Starting to build the sorter class

--- build/index.js
+++ build/index.js
@@ -1,5 +1,11 @@
 "use strict";
-console.log('testing');
-var logSomething = function () {
-    console.log('testing tsc');
-};
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () { };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
