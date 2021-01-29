9181dfbe423fde285df72ad6b6e08dd05e084e3e
Chris Patrick <atomize101@gmail.com>
2021-01-29 12:20:39 -0500

    Wiring it up to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 /* 
 const numbersCollection = new NumbersCollection([10, 3, -5, 50]);
@@ -9,7 +10,19 @@ sorter.sort();
 console.log(numbersCollection.data);
  */
 
+/* 
 const charactersCollection = new CharactersCollection('Xaayb');
 const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(charactersCollection.data);
+ */
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
